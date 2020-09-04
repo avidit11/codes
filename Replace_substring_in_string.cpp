fc07a05a04a84ccf0ecbb9311233f7c8be4216bf
ritiksha000 <47916412+ritiksha000@users.noreply.github.com>
2020-09-04 19:17:20 +0530

    Add files via upload
    WAP to replace “15 august” with “26 january” and  “independence” with “republic” in the string “15 august is independence day”

---
+++ Replace_substring_in_string.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a="15 August is Independence Day ";
+    string w="";
+    int j=0;
+    for(int i=0;i<a.size();i++){
+        if(a[i]!=' '){
+            w=w+a[i];
+        }
+        else{
+            if(w=="15"){
+                a.replace(j,w.size(),"26");
+            }
+            else if(w=="August"){
+                a.replace(j,w.size(),"January");
+            }
+            else if(w=="Independence"){
+                a.replace(j,w.size(),"Republic ");
+            }
+            w="";
+            j=i+1;
+
+        }
+    }
+    cout<<a;
+return 0;
+}
