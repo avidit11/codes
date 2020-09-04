79c74abe8bcc8fb2bfbd3dc791103c8452bf160b
ritiksha000 <47916412+ritiksha000@users.noreply.github.com>
2020-09-04 19:18:22 +0530

    Update Replace_substring_in_string.cpp

--- Replace_substring_in_string.cpp
+++ Replace_substring_in_string.cpp
@@ -1,3 +1,4 @@
+//WAP to replace “15 august” with “26 january” and  “independence” with “republic” in the string “15 august is independence day” 
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
